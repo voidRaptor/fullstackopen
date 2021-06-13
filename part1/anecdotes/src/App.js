47b0097310c5d9696fa3631c0a5630cc94f83312
Severi Kujala <severi.kujala@hotmail.com>
2021-06-13 23:32:10 +0300

    complete part 1 ex1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,9 +1,56 @@
 import React, { useState } from 'react'
 
-const randomMixMax = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
+
+const Daily = ({setters, values}) => {
+  const setSelected = setters[0]
+  const setPoints = setters[1]
+  const anecdotes = values[0]
+  const selected = values[1]
+  const points = values[2]
+
+
+  const vote = (index) => {
+    const copy = [...points]
+    copy[index] += 1
+    setPoints(copy)
+  }
+
+
+  const randomMixMax = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+
+      <button onClick={() => vote(selected)}>vote </button>
+      <button onClick={() => setSelected( randomMixMax(0, anecdotes.length-1) )} >next</button>
+    </>
+  )
+
+
+}
+
+
+const MostVotes = ({anecdotes, points}) => {
+
+  const max = Math.max(...points)
+  const mostVotesIndex = points.findIndex(item => item === max)
+
+  return (
+    <>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVotesIndex]}</p>
+      <p>has {points[mostVotesIndex]} votes</p>
+    </>
+  )
+
 }
 
 
@@ -21,20 +68,13 @@ const App = () => {
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = useState( Array(anecdotes.length).fill(0) )
 
-  const vote = (index) => {
-    const copy = [...points]
-    copy[index] += 1
-    setPoints(copy)
-  }
-
+  const values = [anecdotes, selected, points]
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {points[selected]} votes</p>
+      <Daily setters={[setSelected, setPoints]} values={values}/>
 
-      <button onClick={() => vote(selected)}>vote </button>
-      <button onClick={() => setSelected( randomMixMax(0, anecdotes.length-1) )} >next</button>
+      <MostVotes anecdotes={anecdotes} points={points}/>
     </div>
   )
 }
