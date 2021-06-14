a8d4086fa17da4d3dbb130871f7ad8a3b910c04c
Severi Kujala <severi.kujala@hotmail.com>
2021-06-14 23:26:06 +0300

    complete part 2 ex2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+
+const Header = (props) => {
+  return ( <h1>{props.course}</h1> )
+}
+
+
+const Part = (props) => {
+  return ( <p>{props.title} {props.count}</p> )
+}
+
+
+const Content = (props) => {
+
+  return (
+    <div>
+      {
+        props.parts.map((item) => (
+          <Part key={item.id} title={ item["name"] } count={ item["exercises"] } />
+        ))
+      }
+    </div>
+  )
+
+}
+
+
+/*
+const Total = (props) => {
+  let total = 0
+
+  for (let i=0; i<props.parts.length; ++i) {
+    total += props.parts[i]["exercises"]
+  }
+
+  return ( <p>Number of exercises {total}</p> )
+}
+*/
+
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+    {/*<Total parts={course.parts}/>*/}
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14,
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App;
+
