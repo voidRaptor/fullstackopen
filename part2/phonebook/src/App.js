ce9b910e8257ad37c8504fe11f1d41142731fedd
Severi Kujala <severi.kujala@hotmail.com>
2021-06-17 23:45:21 +0300

    complete part2 ex2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Filter = ({handler, filter}) => {
@@ -37,16 +38,22 @@ const NumberList = ({filterResults}) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ filterResults, setFilterResults ] = useState(persons)
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        //setPersons( persons.concat(response.data) )
+        setPersons(response.data)
+      })
+
+  }, [])
 
 
   // Setting the state is async and last inputted char might not be included in event.
