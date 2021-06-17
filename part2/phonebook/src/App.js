bb7a6bf7db7a34c1a3672e82f9c733788925b031
Severi Kujala <severi.kujala@hotmail.com>
2021-06-17 20:52:55 +0300

    complete part 2 ex2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 const App = () => {
-  const [ persons, setPersons] = useState([ { name: 'Arto Hellas', number: "040 111 2233" } ])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ filterResults, setFilterResults ] = useState(persons)
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
 
 
   const onNameChange = (event) => {
@@ -14,6 +21,21 @@ const App = () => {
   const onNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const onFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+
+  // Setting the state is async and last inputted char might not be included in event.
+  // This is called after the state is set.
+  useEffect(() => {
+    // case insensitive
+    const filtered = persons.filter(item => item.name.toLowerCase().includes(filter.toLowerCase()))
+    console.log(filter, filtered)
+    setFilterResults(filtered)
+
+  }, [filter, persons])
 
   const onSubmit = (event) => {
     event.preventDefault() // prevent redirection
@@ -33,6 +55,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+      filter shown with <input onChange={onFilterChange} value={filter}/>
+      </div>
+
+      <h2>Add new</h2>
       <form onSubmit={onSubmit} >
         <div> name: <input onChange={onNameChange} value={newName}/> </div>
         <div> number: <input onChange={onNumberChange} value={newNumber}/> </div>
@@ -40,9 +67,10 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       {
-        persons.map(item => <p key={item.name}>{item.name} {item.number}</p>)
+        filterResults.map(item => <p key={item.name}>{item.name} {item.number}</p>)
       }
     </div>
   )
